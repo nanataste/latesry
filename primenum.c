8c496a7c4b485e72f078ecded43ecf9b9890542a
nanaba <102130459+nanataste@users.noreply.github.com>
2026-06-30 12:41:10 +0900

    Add prime number checking program in C

---
+++ primenum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n < 2)
+        return 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+
+    printf("정수 입력: ");
+    scanf("%d", &n);
+
+    if (isPrime(n))
+        printf("%d는 소수입니다.\n", n);
+    else
+        printf("%d는 소수가 아닙니다.\n", n);
+
+    return 0;
+}
